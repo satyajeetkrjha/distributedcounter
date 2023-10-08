f422c52655c9cae24b6d28746701ddb641250af6
satyajeetkrjha <satyajeet@satyajeets-MacB.local>
2023-10-08 03:15:02 -0500

    current version without locks

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	_ "embed"
+	"errors"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
 const fileName = "/tmp/messages"
+
+var counterFileName = "/tmp/counter"
 
 // this is max amount of bytes for unix os
 const maxAtomicAppendSize = 512
@@ -29,17 +33,64 @@ type IndexData struct {
 type webHandler struct {
 }
 
+func (h *webHandler) writeMessage(msg string) error {
+	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(msg + "\n"); err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (h *webHandler) ReadCounterValue() (count int, err error) {
+	counterContents, err := ioutil.ReadFile(counterFileName)
+	if errors.Is(err, os.ErrNotExist) {
+		return 0, nil
+	} else if err != nil {
+		return 0, err
+	}
+
+	count, _ = strconv.Atoi(strings.TrimSpace(string(counterContents)))
+	return count, nil
+}
+
+func (h *webHandler) incrementCounter() error {
+	count, err := h.ReadCounterValue()
+	if err != nil {
+		return err
+	}
+	f, err := os.OpenFile(counterFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := fmt.Fprintf(f, "%d\n", count+1); err != nil {
+		return err
+	}
+	return nil
+
+}
+
 func (h *webHandler) helloWorld(w http.ResponseWriter, req *http.Request) {
 	contents, err := ioutil.ReadFile(fileName)
-	if err != nil {
+	if err != nil && !os.IsNotExist(err) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	count, err := h.ReadCounterValue()
 	messages := strings.Split(strings.TrimSpace(string(contents)), "\n")
 	fmt.Println(messages)
+	fmt.Println(count)
 
 	indexTPL.Execute(w, &IndexData{
-		Count:    len(messages),
+		Count:    count,
 		Messages: messages,
 	})
 }
@@ -57,17 +108,18 @@ func (h *webHandler) PostMessage(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "Message too long", http.StatusBadRequest)
 		return
 	}
-	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer f.Close()
 
-	if _, err := f.WriteString(msg + "\n"); err != nil {
+	if err := h.writeMessage(msg); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	// increment counter
+	if err := h.incrementCounter(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	http.Redirect(w, req, "/", http.StatusFound)
 
 }
